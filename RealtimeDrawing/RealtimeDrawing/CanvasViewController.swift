5ac2c1f17fcd725665eeb3f5eb8692081b736994
Jan Misar <jan.misar@ackee.cz>
2020-11-27 10:59:24 +0000

    Add new RealtimeDrawing project

---
+++ RealtimeDrawing/RealtimeDrawing/CanvasViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CanvasViewController.swift
+//  RealtimeDrawing
+//
+//  Created by Jan Misar on 27/11/2020.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
