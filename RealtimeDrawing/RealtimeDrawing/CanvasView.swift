2998d289ab944f72af8f92eca3bdd245ea74e544
Jan Misar <jan.misar@ackee.cz>
2020-11-27 11:14:17 +0000

    Add canvasView

---
+++ RealtimeDrawing/RealtimeDrawing/CanvasView.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasView.swift
+//  RealtimeDrawing
+//
+//  Created by Jan Misar on 27/11/2020.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    
+}
--- RealtimeDrawing/RealtimeDrawing/CanvasViewController.swift
+++ RealtimeDrawing/RealtimeDrawing/CanvasViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: CanvasView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
